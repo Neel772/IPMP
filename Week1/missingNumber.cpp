239ea4a5c3fcede56cbd0e17a4f85dd6394b3f90
Neelakanta <153833919+Neel772@users.noreply.github.com>
2023-12-19 11:34:45 +0530

    Update missingNumber.cpp

--- Week1/missingNumber.cpp
+++ Week1/missingNumber.cpp
@@ -21,3 +21,46 @@ int main(){
     cout<<missing(arr,size);
     return 0;
 }
+
+//optimised version
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+  public:
+    int missingNumber(vector<int>& array, int n) {
+        // Your code goes here
+        int sum =0;
+        for(int i: array){
+            sum += i;
+        }
+        return (n*(n+1)/2 - sum);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.missingNumber(array, n) << "\n";
+    }
+    return 0;
+}
